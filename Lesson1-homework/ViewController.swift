753ed6263c1141c251a69c68560fb2f9dbfc91be
Vic <ji3g4vic03@gmail.com>
2022-11-11 11:03:26 +0800

    Initial Commit

---
+++ Lesson1-homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson1-homework
+//
+//  Created by 王衍智 on 2022/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
